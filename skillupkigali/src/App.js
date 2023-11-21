0047bf514d19341b0326239f478094a52ecc839a
NelHim <n.himbaza@alustudent.com>
2023-11-21 22:49:16 +0200

    components

--- skillupkigali/src/App.js
+++ skillupkigali/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-blue-400 text-center text-4xl font-bold">
-        Skill Up Kigali
+        Skill Up Kigali App
       </h1>
     </div>
   );
